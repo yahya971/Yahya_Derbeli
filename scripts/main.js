b87fa35c4d025d3211dc7cadd22e5b277366882c
yahya971 <derbeliy97@gmail.com>
2023-01-18 00:03:55 +0100

    New_Commit

--- scripts/main.js
+++ scripts/main.js
@@ -92,3 +92,50 @@
 
 
 })();
+
+/*
+document.getElementById("form-submit").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  fetch('http://localhost:3000/formsubmit', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, email, message })
+  })
+  .then(res => res.text())
+  .then(data => {
+      console.log(data);
+      alert(data);
+  })
+  .catch(error => console.error('Error:', error));
+});
+*/
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
